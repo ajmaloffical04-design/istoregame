81d3f34315eeb0d590263294bd03c222396bd8cf
ajmaloffical04-design <ajmaloffical04@gmail.com>
2026-07-30 16:34:55 +0530

    Hide results and stats until user votes

--- src/components/campaign/CampaignArena.tsx
+++ src/components/campaign/CampaignArena.tsx
@@ -191,7 +191,7 @@ export default function CampaignArena() {
 
         {/* Results Bar */}
         <AnimatePresence>
-          {results && (
+          {voted && results && (
             <motion.div
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
@@ -251,7 +251,7 @@ export default function CampaignArena() {
 
         {/* Stats Section */}
         <AnimatePresence>
-          {results && (
+          {voted && results && (
             <motion.div
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
